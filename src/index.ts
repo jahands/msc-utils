e92d0d2206281b50afe3a5b64630e57616aed998
Jacob Hands <jacob@gogit.io>
2022-09-12 16:59:01 -0500

    Add shuffleArray

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,11 @@ export async function log(data: any) {
     })
 }
 
+export function shuffleArray(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 export * as notion from './notion'
